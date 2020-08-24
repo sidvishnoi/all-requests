382c73a1d11aa40401c6dedc462fabbd50035f14
Sid Vishnoi <sidvishnoi8@gmail.com>
2020-08-25 00:29:34 +0530

    feat: get all HTTP requests at a URL

--- index.js
+++ index.js
@@ -1,5 +1,55 @@
 // @ts-check
+const puppeteer = require("puppeteer");
 
-async function* getSubResources(url, options = {}) {}
+/**
+ * @param {URL} url
+ * @param {object} options
+ * @param {import("puppeteer").DirectNavigationOptions["waitUntil"]} [options.waitUntil]
+ * @param {import("puppeteer").DirectNavigationOptions["timeout"]} [options.timeout]
+ */
+async function* getSubResources(url, options = {}) {
+	const browser = await puppeteer.launch();
+	const page = await browser.newPage();
+	await page.setRequestInterception(true);
+
+	/**
+	 * @typedef {{ type: import('puppeteer').ResourceType, url: URL }} SubResource
+	 * @type {SubResource[]}
+	 */
+	let subResources = [];
+	let resolve;
+	/** @type {Promise<void>} */
+	let promise = new Promise(res => (resolve = res));
+	let done = false;
+	page.on("request", async request => {
+		const subResource = {
+			type: request.resourceType(),
+			url: new URL(request.url()),
+		};
+		subResources.push(subResource);
+		resolve();
+		promise = new Promise(res => (resolve = res));
+		await request.continue();
+	});
+
+	page
+		.goto(url.href, options)
+		.then(response => {
+			if (!response || !response.ok()) {
+				const reason = response ? `. HTTP ${response.status()}` : "";
+				throw new Error(`Failed to navigate to ${url}${reason}`);
+			}
+		})
+		.finally(async () => {
+			done = true;
+			await browser.close();
+		});
+
+	while (!done) {
+		await promise;
+		yield* subResources;
+		subResources = [];
+	}
+}
 
 module.exports = { getSubResources };
